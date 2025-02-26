ab1dd9f41414e8d6301b1b42d0e4882d15ddcf85
Lucas Marques <lucas7bm@hotmail.com>
2025-02-26 18:43:12 +0000

    finaliza endpoint de `status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,12 +3,15 @@ import database from "infra/database.js";
 async function status(request, response) {
   const serverVersion = (await database.query("SHOW server_version;")).rows[0]
     .server_version;
-  const maxConnections = (await database.query("SHOW max_connections;")).rows[0]
-    .max_connections;
-  const connectionsInUse = (
-    await database.query(
-      "SELECT COUNT(*) AS connections_in_use FROM pg_stat_activity",
-    )
+  const maxConnections = parseInt(
+    (await database.query("SHOW max_connections;")).rows[0].max_connections,
+  );
+
+  const openedConnections = (
+    await database.query({
+      text: "SELECT COUNT(*)::int AS connections_in_use FROM pg_stat_activity WHERE datname = $1;",
+      values: [process.env.POSTGRES_DB],
+    })
   ).rows[0].connections_in_use;
   const updatedAt = new Date().toISOString();
 
@@ -18,7 +21,7 @@ async function status(request, response) {
       database: {
         version: serverVersion,
         max_connections: maxConnections,
-        connections_in_use: connectionsInUse,
+        opened_connections: openedConnections,
       },
     },
   });
